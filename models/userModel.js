a3d768b5912769de3472b401fb6d2c9142017421
Ianka Karúzia <iankakaruzia@hotmail.com>
2019-08-26 19:41:04 -0300

    creating user model and creating auth controller

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your name'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      message: 'Passwords are not the same',
+      validator: function(el) {
+        return el === this.password;
+      }
+    }
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
