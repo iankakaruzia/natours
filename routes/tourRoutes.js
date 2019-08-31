6fd0d7636040a978d5c64793a004697ac7989040
Ianka Karúzia <iankakaruzia@hotmail.com>
2019-08-30 21:48:25 -0300

    adding nested endpoints for the reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/')
   .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
