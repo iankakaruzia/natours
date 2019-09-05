891254177cb7cd4a2248af8714dfe9905a01dc3d
Ianka Karúzia <iankakaruzia@hotmail.com>
2019-09-05 15:03:25 -0300

    added heroku config

--- server.js
+++ server.js
@@ -40,3 +40,12 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  // eslint-disable-next-line no-console
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    // eslint-disable-next-line no-console
+    console.log('Process terminated');
+  });
+})
